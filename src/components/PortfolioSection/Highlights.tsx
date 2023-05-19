92f644c3e9e9ccf38ad9412f3d040fdc01c5e367
Bruno Lima <brunolima.cad@gmail.com>
2023-05-19 02:47:55 -0600

    added missing key prop to iterating ui

--- src/components/PortfolioSection/Highlights.tsx
+++ src/components/PortfolioSection/Highlights.tsx
@@ -11,7 +11,7 @@ const Highlights: React.FC<Props> = ({ highlights }) => (
       highlights && highlights.length && (
         <div className="flex gap-4 flex-wrap select-none">
           {highlights.map(highlight => (
-            <div className='px-3 py-1 flex gap-3 items-center rounded-full border w-fit min-w-fit' style={{ borderColor: 'var(--primary)' }}>
+            <div key={JSON.stringify(highlight)} className='px-3 py-1 flex gap-3 items-center rounded-full border w-fit min-w-fit' style={{ borderColor: 'var(--primary)' }}>
               {highlight.Icon && <highlight.Icon />}
               <p>{highlight.text}</p>
             </div>
